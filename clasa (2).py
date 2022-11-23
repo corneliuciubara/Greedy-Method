5eb231e53093dbb19d9b239138e4670d48a606da
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-11-23 11:31:19 +0200

    Update clasa (2).py

--- clasa (2).py
+++ clasa (2).py
@@ -1,47 +1,35 @@
-def fractional_knapsack(value, weight, capacity):
-    """Return maximum value of items and their fractional amounts.
- 
-    (max_value, fractions) is returned where max_value is the maximum value of
-    items with total weight not more than capacity.
-    fractions is a list where fractions[i] is the fraction that should be taken
-    of item i, where 0 <= i < total number of items.
- 
-    value[i] is the value of item i and weight[i] is the weight of item i
-    for 0 <= i < n where n is the number of items.
- 
-    capacity is the maximum weight.
-    """
-    # index = [0, 1, 2, ..., n - 1] for n items
+def rucsac(value, weight, capacity):
+
     index = list(range(len(value)))
-    # contains ratios of values to weight
+    
     ratio = [v/w for v, w in zip(value, weight)]
-    # index is sorted according to value-to-weight ratio in decreasing order
+    
     index.sort(key=lambda i: ratio[i], reverse=True)
  
-    max_value = 0
+    valoarea_maxima = 0
     fractions = [0]*len(value)
     for i in index:
         if weight[i] <= capacity:
             fractions[i] = 1
-            max_value += value[i]
+            valoarea_maxima += value[i]
             capacity -= weight[i]
         else:
             fractions[i] = capacity/weight[i]
-            max_value += value[i]*capacity/weight[i]
+            valoarea_maxima += value[i]*capacity/weight[i]
             break
  
-    return max_value, fractions
+    return valoarea_maxima, fractions
  
  
 n = int(input('Numarul de obiecte din rucsac: '))
-value = input('Enter the values of the {} item(s) in order: '
+value = input('Introduceți volumele obiectului(obiectelor) {} în ordine: '
               .format(n)).split()
 value = [int(v) for v in value]
-weight = input('Enter the positive weights of the {} item(s) in order: '
+weight = input('Introduceți ponderile pozitive ale celor {} obiecte în ordine: '
                .format(n)).split()
 weight = [int(w) for w in weight]
-capacity = int(input('Volumul maxim: '))
+capacitatea = int(input('Volumul maxim: '))
  
-max_value, fractions = fractional_knapsack(value, weight, capacity)
-print(':', max_value)
-print('The fractions in which the items should be taken:', fractions)
+valoarea_maxima, fractions = rucsac(value, weight, capacitatea)
+print(':', valoarea_maxima)
+print('Fracțiile în care ar trebui luate obiecrele: ', fractions)
